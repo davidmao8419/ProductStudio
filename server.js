7775eddd4b090d3d5b5d465c190959891525e84d
wei0496 <wei0496@bu.edu>
2018-10-30 17:36:23 -0400

    first push

---
+++ server.js
@@ -0,0 +1,31 @@
+// ./server.js
+// Load dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var morgan       = require('morgan');
+
+var app = express();
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+
+mongoose.connect('mongodb://scotch:scotch@ds041546.mlab.com:41546/cloudinary-instagram', {useMongoClient: true});
+
+
+app.set('port', process.env.PORT || 4000);
+
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Routes
+require('./routes')(app);
+
+var port = app.get('port');
+app.listen(port, function () {
+    console.log('App running at ' + port);
+});
